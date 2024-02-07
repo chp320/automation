93e49cd9d297f539c72ca68a2bd5fe81582ffce6
skyfox83 <chp320@naver.com>
2024-02-07 22:50:39 +0900

    first commit

---
+++ test/src/OpenApp.java
@@ -0,0 +1,8 @@
+package test;
+
+public class OpenApp {
+	public static void main(String[] args) {
+		PrintOut pout = new PrintOut();
+		pout.printMsg("Spring");
+	}
+}
---
+++ test/src/PrintOut.java
@@ -0,0 +1,7 @@
+package test;
+
+public class PrintOut {
+	public void printMsg(String msg) {
+		System.out.println("Hello " + msg + "!");
+	}
+}
